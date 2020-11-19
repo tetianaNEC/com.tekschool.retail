f36b3780d96c90eb86339ff7f9f00d5373f2d502
tetianaNEC <tetiana.nec@gmail.com>
2020-11-19 03:57:03 -0500

    last

--- src/test/java/stepDefinitions/Initializer.java
+++ src/test/java/stepDefinitions/Initializer.java
@@ -7,7 +7,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import core.Base;
@@ -35,13 +35,11 @@ public class Initializer extends Base {
 		// Firefox), because it is faster and goes directly to the needed case
 		// we use WebdriverManager to replace System.setProperty(driver and executable
 		// file)
-		ChromeOptions chromeOptions = new ChromeOptions();
+		
 		String browser = getBrowserName();
 		switch (browser) {
 		case "chrome":
-			
 			WebDriverManager.chromedriver().setup();
-			chromeOptions.addArguments("window-size=1936,1056");
 			driver = new ChromeDriver();
 			break;
 		case "firefox":
@@ -53,15 +51,12 @@ public class Initializer extends Base {
 			driver = new InternetExplorerDriver();
 			break;
 		default:
-			
-		
 			WebDriverManager.chromedriver().setup();
-			chromeOptions.addArguments("window-size=1936,1056");
 			driver = new ChromeDriver();
 		
 
-         //Dimension d = new Dimension(2560,1440);
-		driver.manage().window();//.setSize(d);
+        Dimension d = new Dimension(2560,1440);
+		driver.manage().window().setSize(d);
 		driver.manage().timeouts().pageLoadTimeout(getPageLoadTimeOut(), TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(getImpWait(), TimeUnit.SECONDS);
 	}
